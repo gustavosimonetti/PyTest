302beee37740220a8a1393e75fe2a4135f93e018
gustavosimonetti <63595870+gustavosimonetti@users.noreply.github.com>
2021-05-07 21:20:37 -0400

    Update caesarCipher.py

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,9 +1,17 @@
 text = "Python Rlz!"
 s = 4
 result = ""
+mode='enc' # modo enc/dec - encrypt/decrypt
+
+if mode == 'dec':
+ s *= -1
  
 # traverse text
 for i in range(len(text)):
+  if text[i].isalpha():
+    result += text[i]
+    continue
+  
   char = text[i]
  
   # Encrypt uppercase characters
